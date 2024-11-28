d22e29a755116ea510087fa1442b8dbe3ee69cf6
zareenarif <165668465+zareenarif@users.noreply.github.com>
2024-11-28 16:46:38 +0500

    changes done

---
+++ src/components/skills.tsx
@@ -0,0 +1,17 @@
+export const  Skills= () =>{
+    return(
+        <div>
+          <h2 className='mt-18 mb-24 text-center text-4xl font-bold underline text-green-600 hover:text-green-300'>My Skills</h2>
+          <ul>
+            <li className='mb-3 text-2xl text-yellow-500 ml-16'>HTML</li><div className='w-[38%] ml-16 mb-3 h-4 bg-green-800 rounded-2xl'></div>
+            <li className='mb-3 text-2xl text-yellow-500 ml-16'>CSS</li><div className='w-[40%] ml-16 mb-3 h-4 bg-green-700 rounded-2xl'></div>
+            <li className='mb-3 text-2xl text-yellow-500 ml-16'>Typescript</li> <div className='w-[45%] ml-16 mb-3 h-4 bg-green-600 rounded-2xl'></div>
+            <li className='mb-3 text-2xl text-yellow-500 ml-16'>React</li><div className='w-[25%] ml-16 mb-3 h-4 bg-yellow-300 rounded-2xl'></div>
+            <li className='mb-3 text-2xl text-yellow-500 ml-16'>Next.js</li><div className='w-[20%] ml-16 mb-3 h-4 bg-yellow-600 rounded-2xl'></div>
+            <li className='mb-3 text-2xl text-yellow-500 ml-16'>TailwindCSS</li><div className='w-[35%] ml-16 mb-3 h-4 bg-pink-600 rounded-2xl'></div>
+            <li className='mb-3 text-2xl text-yellow-500 ml-16'>Statistical Analysis</li><div className='w-[55%] ml-16 mb-3 h-4 bg-green-400 rounded-2xl'></div>
+          </ul> 
+          
+        </div>
+    )
+  }
